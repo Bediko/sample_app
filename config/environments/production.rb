88e0bf3b6249c141b6be069da6de6af6392b9f17
Bediko <bediko@chaosdorf.de>
2012-03-04 23:23:29 +0100

    disable ssl on production, caused by missing credit card

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,6 @@
 SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   
-  #Force all access to the app over SSL, use Strict-Transport-Security,
-  #and use secure cookies.
-  config.force_ssl = true
 
   # Precompile Blueprint CSS
   config.assets.precompile += [ "blueprint/*.css" ]
